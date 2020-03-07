b1a06466d7e0c3ac09747fb13d85ed346ee93628
arthur170707 <apinad97@gmail.com>
2020-03-07 20:58:19 +0100

    raycast de Unity con los mandos sin probar. V1: Lanza dos raycast a la vez desde un mismo controlador; NOT FIXED

--- Raycast Unity con VR/Assets/Scripts/DirectorRaycast.cs
+++ Raycast Unity con VR/Assets/Scripts/DirectorRaycast.cs
@@ -22,56 +22,82 @@ public class DirectorRaycast : MonoBehaviour
     }
 
     // Update is called once per frame
+    int left = 0;
+    int right = 0;
+    int llamadas = 0; 
     void Update()
     {
-        if (Input.GetButtonDown("Use")) // si pulsamos el Input que creamos a nombre de "Use" (tecla "p")
+        //Debug.Log("NOMBRE DEL OBJETO: " + this.name);
+      
+        if (Input.GetButtonDown("right_vr_controller"))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // dibuja un rayo invisible hacia donde apunta el cursor del raton
-            if(Physics.Raycast(ray, out hit)) // ¿Choca ese rayo con algun objeto? 
-            {
-                if(hit.collider != null) // Cogemos el collider (propiedades del cuerpo) del objeto que choca
-                {
-                    //Debug.Log("CHOCA" + i + "veces");
+            //Debug.Log("ENTRO");
+            //Debug.Log("Nombre: " + this.name);
+            //Debug.Log("ENTRO DERECHA"); 
 
-                    //Debug.Log("Collider" + i + " : " + hit.collider.name); 
-                    // Finalmente comprobamos si el cuerpo con el que ha chocado es de alguno de los musicos 
-                    // y si es asi cargamos su pista de audio correspondiente. 
-                    if(hit.collider.name == "Music1")
-                    {
-                        fuenteAudio.clip = music1;
-                        fuenteAudio.Play(); 
-                    }
-                    if (hit.collider.name == "Music2")
-                    {
-                        fuenteAudio.clip = music2;
-                        fuenteAudio.Play();
-                    }
-                    if (hit.collider.name == "Music3")
-                    {
-                        fuenteAudio.clip = music3;
-                        fuenteAudio.Play();
-                    }
-                    if (hit.collider.name == "Music4")
-                    {
-                        fuenteAudio.clip = music4;
-                        fuenteAudio.Play();
-                    }
-                    if (hit.collider.name == "Music5")
-                    {
-                        fuenteAudio.clip = music5;
-                        fuenteAudio.Play();
-                    }
-                    i = i + 1; 
+            throwRaycast(GameObject.Find("Controller (right)"));
+          
+
+        }
+        else if (Input.GetButtonDown("left_vr_controller"))
+        {
+            
+            //Debug.Log("ENTRO IZQUIERDA"); 
+            throwRaycast(GameObject.Find("Controller (left)"));
+        }
+    }
+    void throwRaycast(GameObject gameObject)
+    {
+        RaycastHit hit;
+        
+        //Debug.Log("Nombre del raycast: " + gameObject.name);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // dibuja un rayo invisible hacia donde apunta el cursor del raton
+        Ray ray = new Ray(gameObject.transform.position, gameObject.transform.forward);
+        llamadas += 1;
+        Debug.Log("Llamadas a raycast: " + llamadas);
+        if (Physics.Raycast(ray, out hit)) // ¿Choca ese rayo con algun objeto? 
+        {
+            if (hit.collider != null) // Cogemos el collider (propiedades del cuerpo) del objeto que choca
+            {
+                //Debug.Log("CHOCA" + i + "veces");
+                Debug.Log("nombre: " + gameObject.name + "Choca con: " + hit.collider.name); 
+
+                //Debug.Log("Collider" + i + " : " + hit.collider.name);
+                // Finalmente comprobamos si el cuerpo con el que ha chocado es de alguno de los musicos 
+                // y si es asi cargamos su pista de audio correspondiente. 
+                if (hit.collider.name == "Music1")
+                {
+                    fuenteAudio.clip = music1;
+                    fuenteAudio.Play();
                 }
+                if (hit.collider.name == "Music2")
+                {
+                    fuenteAudio.clip = music2;
+                    fuenteAudio.Play();
+                }
+                if (hit.collider.name == "Music3")
+                {
+                    fuenteAudio.clip = music3;
+                    fuenteAudio.Play();
+                }
+                if (hit.collider.name == "Music4")
+                {
+                    fuenteAudio.clip = music4;
+                    fuenteAudio.Play();
+                }
+                if (hit.collider.name == "Music5")
+                {
+                    fuenteAudio.clip = music5;
+                    fuenteAudio.Play();
+                }
+                i = i + 1;
             }
             else
             {
-                Debug.Log("NO CHOCA" + j + "veces");
-                j = j + 1; 
+                //Debug.Log("NO CHOCA" + j + "veces");
+                //Debug.DrawRay(gameObject.transform.position, gameObject.transform.forward * 1000, Color.black, 1);
+                j = j + 1;
             }
-
-
         }
     }
 }
