67a3e0ca25353997ab89658b386ac775b2ff516f
arturopinar97 <apinar@ucm.es>
2020-05-24 19:51:27 +0200

    V1.9 - Agregado HI5_Interaction_SDK | Reconocimiento de gestos | colliders en gestos de tempo

---
+++ Assets/project/Scripts/BoxState.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System; 
+using Valve.VR;
+using Microsoft.VisualBasic; 
+using HI5.VRCalibration;
+
+
+
+public class BoxState : MonoBehaviour {
+
+private void OnTriggerEnter(Collider other) {
+    Debug.Log("Choco con: " + other.gameObject.name); 
+
+
+
+}
+
+
+
+
+
+
+}
