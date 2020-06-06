cc2d90e272141ec04386e536e925b8725a0d0c29
arturopinar97 <apinar@ucm.es>
2020-06-06 14:08:53 +0200

    V3.1 - Escena Tutorial 2/4 completamente funcional

--- Assets/project/Scripts/StateController2_4v2.cs
+++ Assets/project/Scripts/StateController2_4v2.cs
@@ -23,11 +23,17 @@ private const string LEFT_HAND_TAG = "LeftHand";
 private const string RIGHT_HAND_TAG = "RightHand"; 
 
 private const string RESPONSE_TEXT = "text_response_right_hand_gesture"; 
+private const string SECOND_RESPONSE_TEXT = "text_response_second_line"; 
+
 
 private GameObject textResponseRightHandGesture;
+private GameObject textSecond; 
 private static bool writtenText; 
+private static bool writtenSecondText; 
 private const float LIVE_SECONDS_TEXT = 10f; 
 private static float liveSecondsText; 
+private static float liveSecondsSecondText; 
+
 
 private const float SECONDS_PULSE = 1f; 
 
@@ -38,69 +44,30 @@ private const float SECONDS_PULSE = 1f;
 private static string lastTouched; 
 private static int state; 
 private static bool afterMove; 
-private static bool disableBox3; 
-private List<bool> okStates;
+private static  List<bool> okStates;
 private AudioSource fuenteAudio; 
 
 /* ---------- new after 26/05 init v2 ------------- */ 
 
+
 private const string RIGHT_HAND_NAME = "Human_RightHand"; 
-
-// Detect asynchronus gestures: 
-
-class Vector3Wrapper {
-    bool validValue; 
-    Vector3 vector; 
-
-    public Vector3Wrapper() {
-        validValue = false; 
-    }
-    
-    // getters and setters: 
-    public void setValid(bool value) {
-        this.validValue = value; 
-    }
-    public bool getValid() {
-        return this.validValue; 
-    }
-
-    public void setVector(Vector3 vector) {
-        this.vector = vector; 
-    }
-    public Vector3 getVector() {
-        return this.vector; 
-    }
-}
-private static Vector3Wrapper originalPosition; 
-private static Vector3Wrapper finalPosition; 
-public AudioClip testing; 
-public const float STATIC_BORDER_MOVE = 0.01f; // umbral testeado para 0.05 segundos. 
-private const float TIME_DETECTION_ASYNC_GESTURE = 0.05f;
-private static bool lockTimeAsyncGesture; 
-
-
-
-
-
     
     public void Start() {
         textResponseRightHandGesture = GameObject.Find(RESPONSE_TEXT); 
+        textSecond = GameObject.Find(SECOND_RESPONSE_TEXT); 
         fuenteAudio = GetComponent<AudioSource> ();
         writtenText = false; 
+        writtenSecondText = false; 
        
         activeTimer = false; 
-        disableBox3 = false; 
     
         liveSecondsText = LIVE_SECONDS_TEXT; 
+        liveSecondsSecondText = LIVE_SECONDS_TEXT; 
         /* --------- NEW ---------- */ 
         lastTouched = ""; 
         state = 1; 
         afterMove = false; 
         okStates = new List<bool>(); 
-        /* -------- NEW V2 --------- */ 
-        originalPosition = new Vector3Wrapper(); 
-        finalPosition = new Vector3Wrapper(); 
-        lockTimeAsyncGesture = false; 
 
             
     }
@@ -114,12 +81,8 @@ private static bool lockTimeAsyncGesture;
             activeTimer = true; // los estados no pueden reiniciarse hasta llegar al final. 
             state = 1; 
             fuenteAudio.Play(); 
-            //Debug.Log("Original position: " + originalPosition); 
-            //detectAsynchronusGesture(GameObject.Find(RIGHT_HAND_NAME)); 
 
             yield return new WaitForSecondsRealtime(SECONDS_PULSE); 
-            //detectAsynchronusGesture(GameObject.Find(RIGHT_HAND_NAME)); 
-            //Debug.Log("Final position: " + finalPosition); 
             state = 2; 
             fuenteAudio.Play(); 
             yield return new WaitForSecondsRealtime(SECONDS_PULSE); 
@@ -127,34 +90,15 @@ private static bool lockTimeAsyncGesture;
             
            
         }
-        //StartCoroutine(waiter());
     }
-
-    IEnumerator waiterDetectionAsyncGesture() {
-        if(lockTimeAsyncGesture == false) {
-            lockTimeAsyncGesture = true; 
-            yield return new WaitForSecondsRealtime(TIME_DETECTION_ASYNC_GESTURE);
-            detectAsynchronusGesture(GameObject.Find(RIGHT_HAND_NAME)); 
-            lockTimeAsyncGesture = false; 
-        }
-        
          
 
-    }
+    
     public void Update() {
-        //Debug.Log("---- GUANTE: ------  X" + transform.position.x + "Y: " + transform.position.y +  "Z: " + transform.position.z); 
-        //Debug.Log("---- CUBO: ------ X: " + box1.transform.position.x + "Y: " + transform.position.y + "Z: " + transform.position.z); 
-        //timer(); 
         StartCoroutine(waiter());
-       
         luce(); 
         waitToEraseText(); 
-
-        // --- v2 --- 
-        StartCoroutine(waiterDetectionAsyncGesture()); 
-        
-        // iluminar cubos checkeando cada estado en cada frame. 
-    
+        waitToEraseSecondText(); 
     }
 
     
@@ -175,13 +119,13 @@ private static bool lockTimeAsyncGesture;
             handlerTempo(other); 
         }
         else if(other.tag == LEFT_HAND_TAG) {
-            Debug.Log("Ese gesto se debe realizar con la mano derecha"); 
+            updateResponse("Ese gesto se debe realizar con la mano derecha"); 
         }
     }
     private void handlerTempo(Collider other) { 
-        // 1. Comprobar que la mano no este dentro del la caja ya con anterioridad: 
         if(checkIfColliderAvailable(this.name) == true) { 
-            // 2. Handler del gesto: 
+            Debug.Log("Paso por: " + this.name); 
+            Debug.Log("Ok states tiene " + okStates.Count); 
             switch(this.name) {
                 case "state1": 
                     handlerState1(); // estado 1
@@ -203,27 +147,38 @@ private static bool lockTimeAsyncGesture;
 
     private void handlerState1() {
         if(state == 1) {
-            disableBox3 = false; 
             if(afterMove == false) {
+                Debug.Log("Salgo por afterMove que es " + afterMove); 
                 okStates.Add(true); // comienza el ciclo. 
                 afterMove = true; 
+                
             }
             else {
-                if(!okStates.Contains(false)) { // si todos los movimientos han sido correctos
+                if(okStates.Count == 4) { // si todos los movimientos han sido correctos
                     feedbackGoodGesture(); 
                     okStates.Clear(); // prepara el nuevo ciclo. 
                     okStates.Add(true); 
-                }
+                    Debug.Log("segunda vuelta okStates: " + okStates.Count); 
+                    }
+                    else{
+                        feedbackError("Error, follow the path!"); 
+                        handlerError(); 
+                        okStates.Add(true); 
+                        afterMove = true; 
+                    }
             }
         }
         else{
-            feedbackError("Out of time! Start from box 1"); 
+            feedbackError("Out of time!"); 
             handlerError(); 
+            okStates.Add(true); 
+            afterMove = true; 
         }
     }
     private void handlerCommonBoxes(int s) {
         if(afterMove == false) {
-            Debug.Log("Te has equivocado.Comienza por la caja 1"); 
+            updateSecondText("Start from box 1"); 
+            Debug.Log("me salgo por afterMove de 2"); 
         }
         else{
             if(state == s) {
@@ -231,58 +186,55 @@ private static bool lockTimeAsyncGesture;
                     okStates.Add(true); 
                 }
                 else{
-                    feedbackError("Error, follow the path! Start from box 1"); 
+                    feedbackError("Error, follow the path! "); 
                     handlerError(); 
                 }
             }
             else{
-                feedbackError("Out of time! Start from box 1"); 
+                feedbackError("Out of time! "); 
                 handlerError(); 
             }
         }
     }
 
     private void handlerBox3() {
-        if(disableBox3 == false) { // para el caso cuando bajas de la caja 4 a la 1.
             if(afterMove == false) {
-                Debug.Log("Te has equivocado.Comienza por la caja 1"); 
+                updateSecondText("Start from box 1");  
             }
             else{
                 if(state == 2) {
-                    if(!okStates.Contains(false)) {
+                    if(okStates.Count == 2) {
                         okStates.Add(true); 
                     }
                     else{
-                        feedbackError("Error, follow the path! Start from box 1"); 
+                        feedbackError("Error, follow the path! "); 
                         handlerError();
                     }
                 }
                 else{
-                    feedbackError("Out of time! Start from box 1");  
+                    feedbackError("Out of time! ");  
                     handlerError(); 
                 }
             }
-        }
     }
 
     private void handlerAux2() {
         if(afterMove == false) {
-            Debug.Log(" Te has equivocado. Comienza por la caja 1"); 
+            updateSecondText("Start from box 1");  
         }
         else{
             if(state == 2) {
-                if(!okStates.Contains(false)) {
+                if(okStates.Count == 3) {
                     //afterMove = true; 
-                    disableBox3 = true; // para permitir bajar de la caja 4 a la 1. 
                     okStates.Add(true); 
                 }
                 else{
-                    feedbackError("Error, follow the path! Start from box 1"); 
+                    feedbackError("Error, follow the path! "); 
                     handlerError(); 
                 }
             }
             else{
-                feedbackError("Out of time! Start from box 1");  
+                feedbackError("Out of time! ");  
                 handlerError(); 
             }
         }
@@ -299,7 +251,6 @@ private static bool lockTimeAsyncGesture;
     private void handlerError() {
         okStates.Clear(); 
         afterMove = false; 
-        disableBox3 = false; 
         //lastTouched = ""; // desbloquear collider
     }
 
@@ -326,8 +277,16 @@ private static bool lockTimeAsyncGesture;
     /* --------------------- TEXT RESPONSE HANDLER --------------------------- */ 
     private void updateResponse(String text) {
         this.textResponseRightHandGesture.GetComponent<TextMesh>().text = text;
+        this.textSecond.GetComponent<TextMesh>().text = ""; // reseteas el segundo texto porque el primero es el importante. 
         writtenText = true; 
+        liveSecondsText = LIVE_SECONDS_TEXT; 
          
+    }
+
+    private void updateSecondText(String text) {
+        this.textSecond.GetComponent<TextMesh>().text = text; 
+        writtenSecondText = true; 
+        liveSecondsSecondText = LIVE_SECONDS_TEXT; 
     }
 
     private void waitToEraseText() {
@@ -336,57 +295,24 @@ private static bool lockTimeAsyncGesture;
             liveSecondsText -= Time.deltaTime; 
             //Debug.Log("liveSecondsText: " + liveSecondsText); 
             if(liveSecondsText <= 0) {
-                updateResponse(""); 
+                this.textResponseRightHandGesture.GetComponent<TextMesh>().text = "";
                 writtenText = false; 
-                liveSecondsText = LIVE_SECONDS_TEXT; 
+                //liveSecondsText = LIVE_SECONDS_TEXT; 
                 
             }
         }
     }
 
-
-
-    /* ------------------------- NEW AFTER 26/05 INIT V2 ------------------- */ 
-
-    private void setOriginalPosition(GameObject hand) {
-        originalPosition.setVector(hand.transform.position); 
-        originalPosition.setValid(true); 
-    }
-
-    private void setFinalPosition(GameObject hand) {
-        finalPosition.setVector(hand.transform.position);
-        finalPosition.setValid(true); 
-    }
-
-    private void detectAsynchronusGesture(GameObject positionObject) {
-        if(originalPosition.getValid() == false) {
-            setOriginalPosition(positionObject); 
-        }
-        else if(finalPosition.getValid() == false) {
-            setFinalPosition(positionObject); 
-        }
-        if(originalPosition.getValid() == true && finalPosition.getValid() == true) {
-            float distance = Vector3.Distance(originalPosition.getVector(), finalPosition.getVector()); 
-            //Debug.Log("Distancia entre ambos puntos: " + distance);
-            // reset
-            originalPosition.setValid(false); 
-            finalPosition.setValid(false); 
-            if(distance < STATIC_BORDER_MOVE) {
-                //fuenteAudio.clip = testing; 
-                //fuenteAudio.Play();
-                //Debug.Log("HACIENDO GESTO ASINCRONO");  
-            }
-            else{
-                //Debug.Log("No esta haciendo gesto asincrono"); 
+    private void waitToEraseSecondText() {
+        if(writtenSecondText == true) {
+            liveSecondsSecondText -= Time.deltaTime; 
+            if(liveSecondsSecondText <= 0) {
+                this.textSecond.GetComponent<TextMesh>().text = "";  
+                writtenSecondText = false; 
+                //liveSecondsSecondText = LIVE_SECONDS_TEXT; 
             }
         }
-
-    }   
-
-
-
-
-
+    }
 
 
 }
